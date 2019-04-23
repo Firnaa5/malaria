fc09e0752c04d6b337996c351e8114c37029ddce
Your Name <firnaasfir@gmail.com>
2019-04-23 08:58:19 +0530

    index

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var request = require('request');
+var axios = require('axios');
 var app = express();
 const PORT = 3000;
-var url = 'https://www.ebi.ac.uk/europepmc/webservices/rest/search?query=malaria( FIRST_PDATE:[2013-01-01 TO 2013-12-31])&format=json&synonym=true&pageSize=20&sort=CITED desc';
-
-
+var reqUrl ='https://www.ebi.ac.uk/europepmc/webservices/rest/search';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,29 +16,32 @@ app.listen(PORT, function(req, res){
 	console.log(`Server is running @ ${PORT}`);
 })
 
-
-
 app.get('/getWithYear', malaria);
 
-
-function malaria(req, res){
-	request(url, function(err, response, body){
-		if(!err && response.statusCode == 200){
-			var data = JSON.parse(body);
-			var malaria= []
-			data.resultList.result.forEach(function(Result) {
-  				malaria.push({Title: Result.title, Author: Result.authorString, Year: Result.pubYear, Count: Result.citedByCount});  				
+function malaria(req, res){	
+		axios.get(reqUrl,{ params:{query: req.query.query,
+			start: req.query.start,
+			end: req.query.end,
+			sort: 'CITED desc',
+			pageSize:1,
+			synonym: true,
+			format: 'json' }})
+		.then(function(response){
+			const data = response.data.resultList.result;
+			console.log(data);	
+			data.forEach(function(result) {
+  				var responseData = {Title: result.title, Author: result.authorString, Year: result.pubYear, Count: result.citedByCount};
+  	 			res.write(JSON.stringify(responseData));  				
 			});
-			res.status(200).json(malaria);
-		} else {
-			res.status(500).json({
-				content:'Api request failed'
-			})
-		}
-	})
-
-}
-
+			res.end();			
+		})
+		.catch(function(err){
+			 res.status(500).json({
+			 	content:'Api request failed'
+			 })	
+		})
+	}
+			
 
 
 
